f7ef634e2134044b9f46a9e84a86f5531efe952e
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-04-15 10:04:26 -0500

    arreglo

--- scripts/TallerTypeScript/dataSeries.js
+++
@@ -1,9 +0,0 @@
-import { Series } from './series.js';
-export var dataSeries = [
-    new Series(1, "Breaking Bad", "AMC", 5),
-    new Series(2, "Orange Is the New Black", "Netflix", 6),
-    new Series(3, "Game of Thrones", "HBO", 7),
-    new Series(4, "The Big Bang Theory", "CBS", 12),
-    new Series(5, "Sherlock", "BBC", 4),
-    new Series(6, "A Very English Scandal", "BBC", 2),
-];
