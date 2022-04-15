64fbd4c03e01e214107d17d369883da5887b4b62
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-04-14 22:15:08 -0500

    avance

---
+++ TallerTypeScript/dataSeries.ts
@@ -0,0 +1,15 @@
+import { Series } from './series.js';
+
+export const dataseries = [
+    new Series (1,"Breaking Bad","AMC", 5) ,
+    
+    new Series (2,"Orange Is the New Black", "Netflix", 6),
+    
+    new Series (3, "Game of Thrones","HBO", 7),
+
+    new Series (4, "The Big Bang Theory", "CBS", 12),
+    
+    new Series (5, "Sherlock", "BBC",  4),
+
+    new Series (6, "A Very English Scandal", "BBC", 2),
+  ];
