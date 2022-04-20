01ae1be42f5718b522a6f9a5ea12734f0ac6203a
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-04-20 15:19:30 -0500

    organización de carpetas

---
+++ scripts/series.js
@@ -0,0 +1,13 @@
+var Series = /** @class */ (function () {
+    function Series(id, name, channel, seasons, description, fuente, imagen) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.fuente = fuente;
+        this.imagen = imagen;
+    }
+    return Series;
+}());
+export { Series };
