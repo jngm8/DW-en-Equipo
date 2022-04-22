e515469c3ec2616b9a8cc27cc7c911f43aa6efdd
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-04-21 21:39:05 -0500

    avance

--- main.ts
+++ main.ts
@@ -5,16 +5,18 @@ import { dataSerie } from './dataSeries.js';
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 const seasonsAvg: HTMLElement = document.getElementById("seasonAverage")!;
+let infoSerie: HTMLElement = document.getElementById("infolink")!;
 
-renderCoursesInTable(dataSerie);
+renderSeriesInTable(dataSerie);
 
 seasonsAvg.innerHTML = `${getSeasonsAverage(dataSerie)}`
 
 
-function renderCoursesInTable(series: Series[]): void {
+function renderSeriesInTable(series: Series[]): void {
   console.log('Desplegando series');
   series.forEach((serie) => {
     let trElement = document.createElement("tr");
+    trElement.onclick = () => showInfo(serie);
     trElement.innerHTML = `<td>${serie.id}</td>
                            <td>${serie.name}</td>
                            <td>${serie.channel}</td>
@@ -28,5 +30,33 @@ function getSeasonsAverage(series: Series[]): number {
   let seasonsAverage: number = 0;
   series.forEach((serie) => seasonsAverage = seasonsAverage + serie.seasons);
   return (seasonsAverage/series.length);
+}
+
+if (infoSerie.firstChild != null){
+  infoSerie.removeChild(infoSerie.firstChild);
+}
+
+function showInfo(serie:Series) 
+{ 
+  clearSeriesInTable();
+  let cardElement = document.createElement("div");
+    cardElement.setAttribute("class","card");
+    cardElement.setAttribute("style","width: 18rem;");
+    cardElement.innerHTML =    <img src="${serie.img}" class="card-img-top" alt="imagen">
+                                <div class="card-body">
+                                <h5>${serie.name}</h5>
+                                <p class="card-text">${serie.description}</p>
+                                <a href="url">${serie.link}</a>
+                                </div>;
+    infoSerie.appendChild(cardElement);
+}
+
+function clearSeriesInTable() {
+  while (seriesTbody.hasChildNodes()) {
+    if (seriesTbody.firstChild != null) {
+      seriesTbody.removeChild(seriesTbody.firstChild);
+     
+    }
+  }
 }
 
